9c9c730576e22d105cee4c28106446166274ad2f
igor <igormieski27@gmail.com>
2023-11-30 19:30:50 -0300

    changes

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -6,9 +6,7 @@ export class SocketService {
   private listener: EventEmitter<any> = new EventEmitter();
 
   public constructor() {
-    this.socket = new WebSocket(
-      "ws://backend-neko-bitter-darkness-2830-cool-river-1440.fly.dev/ws"
-    );
+    this.socket = new WebSocket("ws:/fly-builder-dawn-cloud-455.fly.dev/ws");
     this.socket.onopen = (event) => {
       this.listener.emit({ type: "open", data: event });
     };
